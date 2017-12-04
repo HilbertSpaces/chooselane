0d83930665b8a540fac044934d4e8432d914fdc6
mvii <mnstryvii@gmail.com>
2017-12-04 16:22:51 -0500

    now how to visualize this data...

--- choosefront/src/ChampStats.js
+++ choosefront/src/ChampStats.js
@@ -17,10 +17,20 @@ class ChampStats extends React.Component{
     }
   render(){
     const champ =
-      <img className='champimg' src={
-      'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/' +
-      'Aatrox_0' + '.jpg'}>
-    </img>
+      <div>
+        <img className='champimg' src={
+        'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/' +
+        'Volibear_0' + '.jpg'}>
+        </img>
+        <div id='cName' className='cName'>{'Vel\'Koz'}
+          <div className='keyStats'>Key Statistics
+          <div></div>
+            <div className='keyStatName'>Stat Name</div>
+            <div className='keyStatName'>How Often</div>
+            <div className='keyStatName'>% Won when True</div>
+          </div>
+        </div>
+      </div>
     const trans = this.state.mounted ?
       <div  key={3} className='champ'>{champ}</div>: null;
     const trans1 = this.state.mounted ?
