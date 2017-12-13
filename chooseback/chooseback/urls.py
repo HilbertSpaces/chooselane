fecc48957a0eb711e732f5df6fbe0bb74c75d4e1
mvii <mnstryvii@gmail.com>
2017-12-13 13:12:02 -0500

    make api available

--- chooseback/chooseback/urls.py
+++ chooseback/chooseback/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    url(r'^$','django.contrib.staticfiles.views.serve', kwargs={
+            'path': 'hi.html', 'document_root': settings.STATIC_ROOT})
     url(r'^admin/', admin.site.urls),
     url(r'api-auth/', include('rest_framework.urls',
         namespace='rest_framework')),
