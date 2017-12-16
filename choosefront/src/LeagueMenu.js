561e988f1769ad1672fbb66453f3c600e4e3f55d
mvii <mnstryvii@gmail.com>
2017-12-16 16:07:08 -0500

    added leagues, next up: handling events on map

--- choosefront/src/LeagueMenu.js
+++ choosefront/src/LeagueMenu.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react'
 
-const LeagueMenu = () => (
-  <Button.Group>
-    <Button inverted basic color='brown'>Bronze</Button>
-    <Button inverted basic color='teal'>Silver</Button>
-    <Button inverted basic color='yellow'>Gold</Button>
-    <Button inverted basic color='purple'>Platinum ^</Button>
-  </Button.Group>
-)
-
+class LeagueMenu extends Component{
+  state = { activeItem: 'bronze' }
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  render(){
+    const { activeItem } = this.state
+    return(
+      <Button.Group>
+        <Button name='bronze' active={activeItem === 'bronze'} inverted basic color='brown'>Bronze</Button>
+        <Button name='silver' active={activeItem === 'silver'} onClick={this.handleItemClick} inverted basic color='teal'>Silver</Button>
+        <Button name='gold' active={activeItem === 'gold'} onClick={this.handleItemClick} inverted basic color='yellow'>Gold</Button>
+        <Button name='platinum' active={activeItem === 'platinum'} onClick={this.handleItemClick} inverted basic color='violet'>Platinum ^</Button>
+      </Button.Group>
+    )
+  }
+}
 export default LeagueMenu
