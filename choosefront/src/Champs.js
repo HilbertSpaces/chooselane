b60269483d749340333abce69533d07e82a422d5
mvii <mnstryvii@gmail.com>
2018-01-08 15:18:28 -0500

    updating frontend for finish backend needs support data

--- choosefront/src/Champs.js
+++ choosefront/src/Champs.js
@@ -6,13 +6,13 @@ import axios from 'axios';
 class Champs extends React.Component {
   constructor(props) {
     super();
-    this.state = {items: ['Aatrox','Ahri','Akali'/*,'Alistar','Amumu','Anivia','Annie','Ashe','AurelionSol','Azir','Bard','Blitzcrank','Brand','Braum','Caitlyn','Camille','Cassiopeia','Chogath','Corki','Darius','Diana','DrMundo','Draven','Ekko',
-    'Elise','Evelynn','Ezreal','Fiddlesticks','Fiora','Fizz','Galio','Gangplank','Garen','Gnar','Gragas','Graves','Hecarim','Heimerdinger','Illaoi','Ivern','Irelia','Janna','JarvanIV','Jax','Jayce','Jhin','Jinx','Kalista','Karma',
-    'Karthus','Kassadin','Katarina','Kayle','Kayn','Kennen','Khazix',
-    'Kindred','Kled','KogMaw','Leblanc','LeeSin','Leona','Lissandra','Lucian','Lulu','Lux','Malphite','Malzahar','Maokai','MasterYi','MissFortune','Mordekaiser','Morgana','Nami','Nasus','Nautilus','Nidalee','Nocturne','Nunu','Olaf',
-    'Orianna','Ornn','Pantheon','Poppy','Quinn','Rakan','Rammus','RekSai','Renekton','Rengar','Riven','Rumble','Ryze','Sejuani','Shaco','Shen','Shyvana','Singed','Sion','Sivir','Skarner','Sona','Soraka','Swain','Syndra',
-    'TahmKench','Taliyah','Talon','Taric','Teemo','Thresh','Tristana','Trundle','Tryndamere','TwistedFate','Twitch','Udyr','Urgot','Varus','Vayne','Veigar','Velkoz','Vi','Viktor',
-    'Vladimir','Volibear','Warwick','MonkeyKing','Xayah','Xerath','XinZhao','Yasuo','Yorick','Zac','Zed','Ziggs','Zilean','Zoe','Zyra'*/
+    this.state = {items: ['Aatrox','Ahri','Akali','Alistar','Amumu','Anivia','Annie','Ashe','Aurelion Sol','Azir','Bard','Blitzcrank','Brand','Braum','Caitlyn','Camille','Cassiopeia',"Cho'Gath",'Corki','Darius','Diana','Dr. Mundo','Draven','Ekko',
+    'Elise','Evelynn','Ezreal','Fiddlesticks','Fiora','Fizz','Galio','Gangplank','Garen','Gnar','Gragas','Graves','Hecarim','Heimerdinger','Illaoi','Ivern','Irelia','Janna','Jarvan IV','Jax','Jayce','Jhin','Jinx','Kalista','Karma',
+    'Karthus','Kassadin','Katarina','Kayle','Kayn','Kennen',"Kha'Zix",
+    'Kindred','Kled',"Kog'Maw",'LeBlanc','Lee Sin','Leona','Lissandra','Lucian','Lulu','Lux','Malphite','Malzahar','Maokai','Master Yi','Miss Fortune','Mordekaiser','Morgana','Nami','Nasus','Nautilus','Nidalee','Nocturne','Nunu','Olaf',
+    'Orianna','Ornn','Pantheon','Poppy','Quinn','Rakan','Rammus',"Rek'Sai",'Renekton','Rengar','Riven','Rumble','Ryze','Sejuani','Shaco','Shen','Shyvana','Singed','Sion','Sivir','Skarner','Sona','Soraka','Swain','Syndra',
+    'Tahm Kench','Taliyah','Talon','Taric','Teemo','Thresh','Tristana','Trundle','Tryndamere','Twisted Fate','Twitch','Udyr','Urgot','Varus','Vayne','Veigar',"Vel'Koz",'Vi','Viktor',
+    'Vladimir','Volibear','Warwick','Wukong','Xayah','Xerath','Xin Zhao','Yasuo','Yorick','Zac','Zed','Ziggs','Zilean','Zoe','Zyra'
   ], lane: false, data: false, tier:'',loading:false, mounted:false};
   }
 componentWillMount() {
@@ -26,21 +26,85 @@ componentWillMount() {
         league: params.league,
         loading: false,
         mounted:true,
-      })});
+      }, this.fixName, this.adjustList)});
 }
 
+  fixName(name) {
+    if (name=="Vel'Koz"){
+      return "Velkoz"
+    }
+    if (name=="Aurelion Sol"){
+      return "AurelionSol"
+    }
+    if (name=="Cho'Gath"){
+      return "Chogath"
+    }
+    if (name=="Dr. Mundo"){
+      return "DrMundo"
+    }
+    if (name=="Jarvan IV"){
+      return "JarvanIV"
+    }
+    if (name=="Kha'Zix"){
+      return "Khazix"
+    }
+    if (name=="Kog'Maw"){
+      return "KogMaw"
+    }
+    if (name=="LeBlanc"){
+      return "Leblanc"
+    }
+    if (name=="Lee Sin"){
+      return "LeeSin"
+    }
+    if (name=="Master Yi"){
+      return "MasterYi"
+    }
+    if (name=="Miss Fortune"){
+      return "MissFortune"
+    }
+    if (name=="Rek'Sai"){
+      return "RekSai"
+    }
+    if (name=="Tahm Kench"){
+      return "TahmKench"
+    }
+    if (name=="Twisted Fate"){
+      return "TwistedFate"
+    }
+    if (name=="Wukong"){
+      return "MonkeyKing"
+    }
+    if (name=="Xin Zhao"){
+      return "XinZhao"
+    }
+    else {
+      return name
+    }
+  }
 
+  adjustList() {
+    const mappr = this.state.items
+    const roles = {'top':0,'middle':1,'jungle':2,'bottom':3,'support':4}
+    this.state.data && mappr.sort((champ1,champ2) => {
+      return this.state.data[champ1]['win']['gamesWon'][roles[this.state.lane]]/this.state.data[champ1]['win']['total'][roles[this.state.lane]]-
+        this.state.data[champ2]['win']['gamesWon'][roles[this.state.lane]]/this.state.data[champ2]['win']['total'][roles[this.state.lane]]
+    })
+    return mappr.reverse()
+  }
   render() {
     const roles = {'top':0,'middle':1,'jungle':2,'bottom':3,'support':4}
-    const champs = this.state && this.state.data && this.state.items.map((champ, i) => (
+    this.adjustList.bind(this)
+    const lst = this.adjustList()
+    const champs = this.state && this.state.data && lst.map((champ, i) => (
     <Link key={champ} className='lanes' id={champ} to={'/statistics/' + this.state.league +'/' +this.state.lane + '/'+ champ}>
       <div className='cham' key={champ}>
         <img className='champs'
           alt={champ}
           src={'http://ddragon.leagueoflegends.com/cdn/7.24.2/img/champion/' +
-          champ + '.png'}></img>
+          this.fixName(champ) + '.png'}></img>
         <div className='name'>{champ}</div>
-        <div key={champ+'wr'} className='win_rate'>{Math.floor((this.state.data['Ahri']['win']['gamesWon'][roles[this.state.lane]]/this.state.data['Ahri']['win']['total'][1  ])*100).toString() + '%'}</div>
+        <div key={champ+'wr'} className='win_rate'>{(((this.state.data[champ]['win']['gamesWon'][roles[this.state.lane]]/this.state.data[champ]['win']['total'][roles[this.state.lane]])*100)).toFixed(2).toString() + '%'}</div>
       </div>
       </Link>
     ));
@@ -57,7 +121,7 @@ componentWillMount() {
           transitionEnterTimeout={900}
           transitionLeaveTimeout={900}>
           {trans}
-          {this.state.lane && console.log(this.state.lane)}
+          {this.state.lane && console.log(this.state.data)}
         </ReactCSSTransitionGroup>
       </div>
       </ReactCSSTransitionGroup>
