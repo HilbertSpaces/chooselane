83caa326fbec7d73538820fac6e30e5757211d4f
mvii <mnstryvii@gmail.com>
2018-01-08 20:54:50 -0500

    updating frontend for finish backend needs support data

--- choosefront/src/Champs.js
+++ choosefront/src/Champs.js
@@ -30,52 +30,52 @@ componentWillMount() {
 }
 
   fixName(name) {
-    if (name=="Vel'Koz"){
+    if (name==="Vel'Koz"){
       return "Velkoz"
     }
-    if (name=="Aurelion Sol"){
+    if (name==="Aurelion Sol"){
       return "AurelionSol"
     }
-    if (name=="Cho'Gath"){
+    if (name==="Cho'Gath"){
       return "Chogath"
     }
-    if (name=="Dr. Mundo"){
+    if (name==="Dr. Mundo"){
       return "DrMundo"
     }
-    if (name=="Jarvan IV"){
+    if (name==="Jarvan IV"){
       return "JarvanIV"
     }
-    if (name=="Kha'Zix"){
+    if (name==="Kha'Zix"){
       return "Khazix"
     }
-    if (name=="Kog'Maw"){
+    if (name==="Kog'Maw"){
       return "KogMaw"
     }
-    if (name=="LeBlanc"){
+    if (name==="LeBlanc"){
       return "Leblanc"
     }
-    if (name=="Lee Sin"){
+    if (name==="Lee Sin"){
       return "LeeSin"
     }
-    if (name=="Master Yi"){
+    if (name==="Master Yi"){
       return "MasterYi"
     }
-    if (name=="Miss Fortune"){
+    if (name==="Miss Fortune"){
       return "MissFortune"
     }
-    if (name=="Rek'Sai"){
+    if (name==="Rek'Sai"){
       return "RekSai"
     }
-    if (name=="Tahm Kench"){
+    if (name==="Tahm Kench"){
       return "TahmKench"
     }
-    if (name=="Twisted Fate"){
+    if (name==="Twisted Fate"){
       return "TwistedFate"
     }
-    if (name=="Wukong"){
+    if (name==="Wukong"){
       return "MonkeyKing"
     }
-    if (name=="Xin Zhao"){
+    if (name==="Xin Zhao"){
       return "XinZhao"
     }
     else {
@@ -86,11 +86,19 @@ componentWillMount() {
   adjustList() {
     const mappr = this.state.items
     const roles = {'top':0,'middle':1,'jungle':2,'bottom':3,'support':4}
+    var lst = []
     this.state.data && mappr.sort((champ1,champ2) => {
       return this.state.data[champ1]['win']['gamesWon'][roles[this.state.lane]]/this.state.data[champ1]['win']['total'][roles[this.state.lane]]-
         this.state.data[champ2]['win']['gamesWon'][roles[this.state.lane]]/this.state.data[champ2]['win']['total'][roles[this.state.lane]]
     })
-    return mappr.reverse()
+    for (var i=0; i<mappr.length; i++){
+      const num = this.state.data && this.state.data[mappr[i]]['goldEarned']['total'][roles[this.state.lane]]
+      const den = this.state.data && this.state.data[mappr[i]]['sampleSize']
+      if (num/den > .22){
+        lst.push(mappr[i]);
+      }
+    }
+    return lst.reverse()
   }
   render() {
     const roles = {'top':0,'middle':1,'jungle':2,'bottom':3,'support':4}
@@ -121,7 +129,7 @@ componentWillMount() {
           transitionEnterTimeout={900}
           transitionLeaveTimeout={900}>
           {trans}
-          {this.state.lane && console.log(this.state.data)}
+          {this.state.lane && console.log(this.state.data['Ahri']['goldEarned']['total'][roles[this.state.lane]]/this.state.data['Ahri']['sampleSize'])}
         </ReactCSSTransitionGroup>
       </div>
       </ReactCSSTransitionGroup>
