2b67fb46870d87e89e861510ce46d3bbb522b84f
mvii <mnstryvii@gmail.com>
2018-01-12 14:08:29 -0500

    add more legal language

--- choosefront/src/About.js
+++ choosefront/src/About.js
@@ -52,7 +52,7 @@ class About extends Component {
           <br />
           <br />
           <br />
-          <p className='legal'>
+          <p className='legal_full'>
           The LaneSeven Project isn’t endorsed by Riot Games and doesn’t reflect the views or opinions
           of Riot Games or anyone officially involved in producing or managing League of Legends.
           League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.
